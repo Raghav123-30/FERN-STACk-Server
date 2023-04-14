377b3207205aeeff43840d9d54b585a94ecc0973
Raghav123-30 <Raghavendrabhat2018@gmail.com>
2023-04-14 12:10:49 +0530

    Fixed middleware

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const {db} = require("./firebase") 
-const {getDocs, collection} = require("firebase/firestore");
+const {getDocs, collection,addDoc} = require("firebase/firestore");
 const app = express();
 
-const submitHandler = require("./submit");
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(express.json());
 
-app.use(bodyParser.json());
+
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Headers","*");
+res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -18,9 +24,60 @@ app.use((req, res, next) => {
   next();
 });
 
-console.log(submitHandler);
+const { v4: uuidv4 } = require('uuid');
 
-app.post("/api/submit", (req, res) => {
+// app.post('/api/login', (req, res) => {
+//   // Set cookie using res.cookie()
+//   res.cookie('isLoggedin', 'true', { httpOnly: true });
+//   console.log("Cookies",req.cookies);
+  
+//   res.status(200).json({
+//     message: 'SUCCESS'
+//   });
+// });
+
+// // Check auth state route
+
+// app.get('/api/checkAuthState', (req, res) => {
+//   // Set a cookie
+//   res.cookie('myCookie', 'Hello World!', { httpOnly: true });
+
+//   // Log the cookies
+//   console.log(req.cookies);
+//   if(req.cookies.isLoggedin){
+//     res.status(200).json({ message: 'SUCCESS', data:true });
+//   }
+//   else{
+//     res.status(501).json({message:'FAILED', data:false});
+//   }
+
+  
+// });
+
+app.post('/api/login', (req, res) => {
+  // Set cookie using res.cookie()
+  res.cookie('isLoggedIn', 'true', { httpOnly: true,domain: 'http://localhost:3001',sameSite  });
+  console.log("Cookies",req.cookies);
+  
+  res.status(200).json({
+    message: 'SUCCESS'
+  });
+});
+
+
+app.get('/api/checkAuthState', (req, res) => {
+  console.log("Cookies",req.cookies);
+  res.cookie('isLoggedIn', 'true', { httpOnly: true ,domain: 'http://localhost:3001',SameSite: None });
+   console.log(req.cookies);
+  res.json(req.cookies);
+ 
+  
+});
+
+app.post("/api/submitOp", (req, res) => {
+  console.log(req.body);
+});
+app.post("/api/submitOwner", (req, res) => {
   console.log(req.body);
 });
 
@@ -122,27 +179,59 @@ app.get('/api/getAllAddress', async(req,res) => {
     });
   }
 })
-app.post('/api/addloc',async(req,res)=>{
-    const operatorCollection = collection(db,'Location');
-        const body = JSON.parse(req.body);
-        try{
-            await addDoc(operatorCollection,{
-                location: body.location,
-                owner : body.owner,
-                phoneno : body.phoneno,
-                adhar: body.adhar
-            }).then(() => {
-                res.status(200).json({
-                    message :'SUCCESS'
-                })
-            })
-        }catch(error){
-            res.status(500).json({
-                message : 'FAILED'
-            })
-        }
-    }
+app.post('/api/addop',async(req,res)=>{
+  const operatorCollection = collection(db,'Operator');
+ 
+  const body = req.body;
+  console.log("Hi........")
+    
+      console.log(body.fullName, body.address, body.location, body.phone, body.adhar);
+      try{
+          await addDoc(operatorCollection,{
+            fullName: body.fullName,
+            address: body.address,
+            adharNumber: body.adhar,
+            phone: body.phone,
+            location: body.location
+          }).then(() => {
+              console.log("Added successfully")
+              res.status(200).json({
+                  message :'SUCCESS'
+              })
+          })
+      }catch(error){
+          res.status(500).json({
+              message : 'FAILED'
+          })
+      }
+  }
 )
+app.post('/api/addloc',async(req,res)=>{
+  const operatorCollection = collection(db,'Location');
+  const body = req.body;
+  console.log(body);
+     
+   
+  try{
+      await addDoc(operatorCollection,{
+          location: body.address,
+          owner : body.fullName,
+          phoneno : body.phone,
+          adhar: body.adhar
+      }).then(() => {
+        console.log("Added successfully")
+          res.status(200).json({
+            
+              message :'SUCCESS'
+          })
+      })
+  }catch(error){
+      res.status(500).json({
+          message : 'FAILED'
+      })
+  }
+});
+
 app.post("/api/updateop", async (req, res) => {
   try {
     const body = JSON.parse(req.body);
