3eb5a03dd1a7c48c2578af50dae8b24739f98219
Raghav123-30 <Raghavendrabhat2018@gmail.com>
2023-04-16 18:36:28 +0530

    Added authentication,fixed middleware related issues

--- server/app.js
+++ server/app.js
@@ -1,77 +1,56 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const {db} = require("./firebase") 
-const {getDocs, collection,addDoc} = require("firebase/firestore");
+const { db } = require("./firebase");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const secret = "pequrelInterns@$!!8*";
+const { getDocs, collection, addDoc } = require("firebase/firestore");
 const app = express();
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(express.json());
-
-
+app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Headers","*");
-res.header('Access-Control-Allow-Credentials', true);
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With,Content-Type, Accept, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   next();
 });
 
-const { v4: uuidv4 } = require('uuid');
+app.post("/api/login", (req, res) => {
+  console.log(req.body);
 
-// app.post('/api/login', (req, res) => {
-//   // Set cookie using res.cookie()
-//   res.cookie('isLoggedin', 'true', { httpOnly: true });
-//   console.log("Cookies",req.cookies);
-  
-//   res.status(200).json({
-//     message: 'SUCCESS'
-//   });
-// });
+  // In a real app, you should validate the user's credentials here
 
-// // Check auth state route
+  const username = "raghav";
+  const payload = { username }; // You can include additional data here
+  const token = jwt.sign(payload, secret, { expiresIn: "1h" });
 
-// app.get('/api/checkAuthState', (req, res) => {
-//   // Set a cookie
-//   res.cookie('myCookie', 'Hello World!', { httpOnly: true });
-
-//   // Log the cookies
-//   console.log(req.cookies);
-//   if(req.cookies.isLoggedin){
-//     res.status(200).json({ message: 'SUCCESS', data:true });
-//   }
-//   else{
-//     res.status(501).json({message:'FAILED', data:false});
-//   }
-
-  
-// });
-
-app.post('/api/login', (req, res) => {
-  // Set cookie using res.cookie()
-  res.cookie('isLoggedIn', 'true', { httpOnly: true,domain: 'http://localhost:3001',sameSite  });
-  console.log("Cookies",req.cookies);
-  
-  res.status(200).json({
-    message: 'SUCCESS'
-  });
+  res.status(200).json({ token: token, message: "SUCCESS" });
 });
 
+app.post("/api/checkAuthState", (req, res) => {
+  const token = req.body.token;
+  console.log(token);
 
-app.get('/api/checkAuthState', (req, res) => {
-  console.log("Cookies",req.cookies);
-  res.cookie('isLoggedIn', 'true', { httpOnly: true ,domain: 'http://localhost:3001',SameSite: None });
-   console.log(req.cookies);
-  res.json(req.cookies);
- 
-  
+  if (!token) {
+    return res.sendStatus(401); // Unauthorized
+  }
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return res.sendStatus(403); // Forbidden
+    }
+
+    res.sendStatus(200); // OK
+  });
 });
 
 app.post("/api/submitOp", (req, res) => {
@@ -85,8 +64,7 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from express" });
 });
 
-
-app.get("/api/listowners", async (req, res) =>{
+app.get("/api/listowners", async (req, res) => {
   console.log("Hi from API");
   const documents = [];
   const operatorCollectionRef = collection(db, "Location");
@@ -101,7 +79,7 @@ app.get("/api/listowners", async (req, res) =>{
             adharNumber: doc.data().adhar,
             address: doc.data().location,
             season: doc.data().season,
-            cost:doc.data().cost
+            cost: doc.data().cost,
           };
           console.log(document);
           documents.push(document);
@@ -151,7 +129,7 @@ app.get("/api/listop", async (req, res) => {
     });
   }
 });
-app.get('/api/getAllAddress', async(req,res) => {
+app.get("/api/getAllAddress", async (req, res) => {
   console.log("Hi from API");
   const documents = [];
   const operatorCollectionRef = collection(db, "Location");
@@ -160,8 +138,9 @@ app.get('/api/getAllAddress', async(req,res) => {
       .then((snapshots) => {
         snapshots.forEach((doc) => {
           const document = {
-            id : doc.id,
+            id: doc.id,
             location: doc.data().location,
+            numTrays: doc.data().numTrays,
           };
           console.log(document);
           documents.push(document);
@@ -178,57 +157,60 @@ app.get('/api/getAllAddress', async(req,res) => {
       message: "FAILED",
     });
   }
-})
-app.post('/api/addop',async(req,res)=>{
-  const operatorCollection = collection(db,'Operator');
- 
+});
+app.post("/api/addop", async (req, res) => {
+  const operatorCollection = collection(db, "Operator");
+
   const body = req.body;
-  console.log("Hi........")
-    
-      console.log(body.fullName, body.address, body.location, body.phone, body.adhar);
-      try{
-          await addDoc(operatorCollection,{
-            fullName: body.fullName,
-            address: body.address,
-            adharNumber: body.adhar,
-            phone: body.phone,
-            location: body.location
-          }).then(() => {
-              console.log("Added successfully")
-              res.status(200).json({
-                  message :'SUCCESS'
-              })
-          })
-      }catch(error){
-          res.status(500).json({
-              message : 'FAILED'
-          })
-      }
+  console.log("Hi........");
+
+  console.log(
+    body.fullName,
+    body.address,
+    body.location,
+    body.phone,
+    body.adhar
+  );
+  try {
+    await addDoc(operatorCollection, {
+      fullName: body.fullName,
+      address: body.address,
+      adharNumber: body.adhar,
+      phone: body.phone,
+      location: body.location,
+    }).then(() => {
+      console.log("Added successfully");
+      res.status(200).json({
+        message: "SUCCESS",
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "FAILED",
+    });
   }
-)
-app.post('/api/addloc',async(req,res)=>{
-  const operatorCollection = collection(db,'Location');
+});
+app.post("/api/addloc", async (req, res) => {
+  const operatorCollection = collection(db, "Location");
   const body = req.body;
   console.log(body);
-     
-   
-  try{
-      await addDoc(operatorCollection,{
-          location: body.address,
-          owner : body.fullName,
-          phoneno : body.phone,
-          adhar: body.adhar
-      }).then(() => {
-        console.log("Added successfully")
-          res.status(200).json({
-            
-              message :'SUCCESS'
-          })
-      })
-  }catch(error){
-      res.status(500).json({
-          message : 'FAILED'
-      })
+
+  try {
+    await addDoc(operatorCollection, {
+      location: body.address,
+      owner: body.fullName,
+      phoneno: body.phone,
+      adhar: body.adhar,
+    }).then(() => {
+      console.log("Added successfully");
+      res.status(200).json({
+        message: "SUCCESS",
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "FAILED",
+    });
   }
 });
 
@@ -254,22 +236,22 @@ app.post("/api/updateop", async (req, res) => {
     });
   }
 });
-app.post('/api/deleteop',async(req,res) => {
-    const body = JSON.parse(req.body);
-        const id = body.id;
-        const docRef = doc(db, "Operator", body.id);
-        try{
-            await deleteDoc(docRef).then(() => {
-                res.status(200).json({
-                    message :'SUCCESSFULLY DELETED THE OPERATOR'
-                })
-            })
-        }catch{
-            res.status(500).json({
-                message : "SOMETHING WENT WRONG"
-            })
-        }
-})
+app.post("/api/deleteop", async (req, res) => {
+  const body = JSON.parse(req.body);
+  const id = body.id;
+  const docRef = doc(db, "Operator", body.id);
+  try {
+    await deleteDoc(docRef).then(() => {
+      res.status(200).json({
+        message: "SUCCESSFULLY DELETED THE OPERATOR",
+      });
+    });
+  } catch {
+    res.status(500).json({
+      message: "SOMETHING WENT WRONG",
+    });
+  }
+});
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
